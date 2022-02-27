0451da45a7692eec6861cdb44ff2a673a76b98cc
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-02-28 00:11:28 +0700

    Feature - Transaction components (template)
    - Item (main page & search page)
    - Detail

---
+++ lib/widgets/transaction.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+// Transaction Item (auth page)
+class AppTransactionItemMain extends StatelessWidget {
+  const AppTransactionItemMain({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+// Transaction Item (search page)
+class AppTransactionItemSearch extends StatelessWidget {
+  const AppTransactionItemSearch({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+// Transaction Detail (from transaction item)
+class AppTransactionDetail extends StatefulWidget {
+  const AppTransactionDetail({Key? key}) : super(key: key);
+
+  @override
+  _AppTransactionDetailState createState() => _AppTransactionDetailState();
+}
+
+class _AppTransactionDetailState extends State<AppTransactionDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
