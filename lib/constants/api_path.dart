f41d201b0f9eae65e5ef21c00d72d968575f0f90
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-03-05 22:13:04 +0700

    Feature - Initial Rest API constants

--- lib/constants/api_path.dart
+++ lib/constants/api_path.dart
@@ -1,2 +1,13 @@
 // API endpoints
-const apiUrl = "localhost";
+
+const _server =
+    "192.168.33.13"; // Replace by Wi-fi ip, which can be viewed in cmd > ipconfig
+const _port1 = "9000";
+const _port2 = "9090";
+const _basePath = "api/v1";
+const _urlRoot1 = "http://$_server:$_port1/$_basePath";
+const _urlRoot2 = "http://$_server:$_port2/$_basePath";
+
+// Export
+const urlSignUp = "$_urlRoot1/signup";
+const urlSignIn = "$_urlRoot1/import";
