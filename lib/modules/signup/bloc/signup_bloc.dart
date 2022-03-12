41c1687c50ebb68bfcfc44511df9859eeead3c7d
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-03-12 11:39:15 +0700

    Feature - Initial BLoC for auth

---
+++ lib/modules/signup/bloc/signup_bloc.dart
@@ -0,0 +1,10 @@
+import 'package:botp_auth/modules/signup/bloc/signup_event.dart';
+import 'package:botp_auth/modules/signup/bloc/signup_state.dart';
+import 'package:botp_auth/modules/signup/repositories/signup_repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+// class SignUpBloc extends Bloc<SignUpEvent, SignUpState> {
+//   final SignUpRepository signUpRepository;
+//
+//   SignUpBloc({required this.signUpRepository}) : super();
+// }
