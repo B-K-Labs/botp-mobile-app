30a5aa365ee26a9f3d26a7f3ca4db90ffd8f3915
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-04-19 10:54:49 +0700

    refactor

--- lib/common/repositories/settings_repository.dart
+++ lib/common/repositories/settings_repository.dart
@@ -1,19 +1,20 @@
-import 'dart:convert';
-import 'package:botp_auth/constants/api_path.dart';
 import 'package:botp_auth/common/models/settings_model.dart';
+import 'package:botp_auth/configs/environment/environment.dart';
 import 'package:botp_auth/utils/services/rest_api_service.dart';
 import 'package:http/http.dart' as http;
-import 'package:universal_html/html.dart';
 
 class SettingsRepository {
+
+  final apiUri = Environment().config
+
   Future<KYCResponseModel> kyc(String bcAddress, String fullName, int age,
       String gender, String debitor) async {
     final data =
         KYCRequestModel(bcAddress, fullName, age, gender, debitor).toJson();
-    // http.Response result = await post(BotpAPI.userKycUrl.toString(), data);
-    // if (result.statusCode == HttpStatus.ok) {
-    //   return KYCResponseModel.fromJson(json.decode(result.body));
-    // }
+    http.Response result = await post(BotpAPI.userKycUrl.toString(), data);
+    if (result.statusCode == HttpStatus.ok) {
+      return KYCResponseModel.fromJson(json.decode(result.body));
+    }
     throw Exception("Failed to do KYC");
   }
 }
