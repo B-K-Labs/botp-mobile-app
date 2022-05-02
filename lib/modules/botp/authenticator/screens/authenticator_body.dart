1df181d953e210bf169a8c4603102e6552cfc376
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-05-02 12:30:18 +0700

    update: consistent logo size

--- lib/modules/botp/authenticator/screens/authenticator_body.dart
+++ lib/modules/botp/authenticator/screens/authenticator_body.dart
@@ -210,8 +210,8 @@ class _AuthenticatorBodyState extends State<AuthenticatorBody> {
                   child: Center(
                       child: Column(mainAxisSize: MainAxisSize.min, children: [
                   SizedBox(
-                    height: 120.0,
-                    width: 120.0,
+                    height: 80.0,
+                    width: 80.0,
                     child: Image.asset(
                         "assets/images/logo/botp_logo_disabled.png",
                         scale: 1,
