1367639f5d772e58714d835770983c7de33c5e32
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-05-05 00:25:45 +0700

    update: truncate message with newline replacement

--- lib/utils/helpers/transaction.dart
+++ lib/utils/helpers/transaction.dart
@@ -1,9 +1,14 @@
 DateTime dateTimeFromSecondsSinceEpoch(String millisecondsSinceEpoch) =>
     DateTime.fromMillisecondsSinceEpoch(int.parse(millisecondsSinceEpoch));
 
-String shortenNotifyMessage(String notifyMessage) =>
-    // Example: "[khiem20tc] Facebook need your confirmation to your account password" would be
-    // "[khiem20tc] Facebook need your confirmation to you..."
-    notifyMessage.length > 40
-        ? notifyMessage.replaceRange(40, notifyMessage.length, '...')
-        : notifyMessage;
+String shortenNotifyMessage(String notifyMessage) {
+  // Example: "[khiem20tc] Facebook need your confirmation to your account password" would be
+  // "[khiem20tc] Facebook need your confirmation to you..."
+  final String puredNotifyMessage =
+      notifyMessage.replaceAll("\n", "").replaceAll("\t", "");
+  const int maxLength = 35;
+  return puredNotifyMessage.length > maxLength
+      ? puredNotifyMessage.replaceRange(
+          maxLength, puredNotifyMessage.length, '...')
+      : puredNotifyMessage;
+}
