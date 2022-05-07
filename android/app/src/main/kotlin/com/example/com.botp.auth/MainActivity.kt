42128b65a995996aad7fdb23098b7f070b9983f1
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-05-07 16:49:11 +0700

    update: config project for iOS and Android

---
+++ android/app/src/main/kotlin/com/example/com.botp.auth/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.botp.auth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
--- android/app/src/main/kotlin/com/example/untitled/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.botp.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
