f639df2429caa20be5764186d1c69e6daa4d482e
Ankun <sytungan@gmail.com>
2024-04-12 22:02:01 +0700

    chore(debug): add debug network log

--- lib/utils/services/rest_service.dart
+++ lib/utils/services/rest_service.dart
@@ -1,7 +1,9 @@
 // RestAPI to remote server
-import 'dart:io';
-import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 Map<String, String> _defaultHeaders = {
   HttpHeaders.contentTypeHeader: "application/json",
@@ -17,28 +19,63 @@ Map<String, String> _combineHeaders(String? token,
           }
         : {..._defaultHeaders, ...headers};
 
+void printRequestInfo(http.Response response, {dynamic body}) {
+  if (kDebugMode) {
+    print('Request Info:');
+    print('URL: ${response.request!.url}');
+    print('Method: ${response.request!.method}');
+    print('Headers: ${response.request!.headers}');
+    print('Body: $body');
+  }
+}
+
+void printResponseInfo(http.Response response) {
+  if (kDebugMode) {
+    print('Response Info:');
+    print('Status Code: ${response.statusCode}');
+    print('Headers: ${response.headers}');
+    print('Body: ${response.body}');
+  }
+}
+
 Future<http.Response> get(url,
-        [token, Map<String, String> otherHeaders = const {}]) async =>
-    await http.get(Uri.parse(url),
-        headers: _combineHeaders(token, otherHeaders));
+    [token, Map<String, String> otherHeaders = const {}]) async {
+  final response = await http.get(Uri.parse(url),
+      headers: _combineHeaders(token, otherHeaders));
+  printRequestInfo(response);
+  printResponseInfo(response);
+  return response;
+}
 
 Future<http.Response> post(url, body,
-        [token, Map<String, String> otherHeaders = const {}]) async =>
-    await http.post(Uri.parse(url),
-        headers: _combineHeaders(token, otherHeaders),
-        body: convert.jsonEncode(body),
-        encoding: convert.Encoding.getByName("utf-8"));
+    [token, Map<String, String> otherHeaders = const {}]) async {
+  final response = await http.post(Uri.parse(url),
+      headers: _combineHeaders(token, otherHeaders),
+      body: convert.jsonEncode(body),
+      encoding: convert.Encoding.getByName("utf-8"));
+  printRequestInfo(response, body: body);
+  printResponseInfo(response);
+  return response;
+}
 
 Future<http.Response> put(url, body,
-        [token, Map<String, String> otherHeaders = const {}]) async =>
-    await http.put(Uri.parse(url),
-        headers: _combineHeaders(token, otherHeaders),
-        body: convert.jsonEncode(body),
-        encoding: convert.Encoding.getByName("utf-8"));
+    [token, Map<String, String> otherHeaders = const {}]) async {
+  final response = await http.put(Uri.parse(url),
+      headers: _combineHeaders(token, otherHeaders),
+      body: convert.jsonEncode(body),
+      encoding: convert.Encoding.getByName("utf-8"));
+  printRequestInfo(response, body: body);
+  printResponseInfo(response);
+  return response;
+}
 
 Future<http.Response> patch(url, body,
-        [token, Map<String, String> otherHeaders = const {}]) async =>
-    await http.patch(Uri.parse(url),
-        headers: _combineHeaders(token, otherHeaders),
-        body: convert.jsonEncode(body),
-        encoding: convert.Encoding.getByName("utf-8"));
+    [token, Map<String, String> otherHeaders = const {}]) async {
+  final response = await http.patch(Uri.parse(url),
+      headers: _combineHeaders(token, otherHeaders),
+      body: convert.jsonEncode(body),
+      encoding: convert.Encoding.getByName("utf-8"));
+  printRequestInfo(response, body: body);
+  printResponseInfo(response);
+  return response;
+}
